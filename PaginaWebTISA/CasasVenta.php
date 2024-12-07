ee897a60746c731ee65e47f439124f70d479efd7
03Gabrielash0 <gabiig8419@gmail.com>
2024-12-07 12:19:10 -0600

    setimo commit

--- PaginaWebTISA/CasasVenta.php
+++ PaginaWebTISA/CasasVenta.php
@@ -11,35 +11,35 @@ $imagenes = array_diff(scandir($directorio), array('..', '.'));
     <title>Casas en Venta</title>
     <style>
         body{
-            font-family: Arial, sans-serif;
-            padding: 20px;
-            display: flex;
-            justify-content: center;
-            align-items: initial;
-            height: 300vh;
-            margin: 0;
-            background-color: #f0f0f0;
+            font-family: Arial, sans-serif !important;
+            padding: 20px !important;
+            display: flex!important;
+            justify-content: center!important;
+            align-items: initial!important;
+            height: 300vh!important;
+            margin: 0!important;
+            background-color: #f0f0f0!important;
         }
         .gallery{
-            width: 850px;
-            height: 80vh;
-            display: flex;
-            flex-direction: column;
-            gap: 10px;
-            padding: 10px;
-            box-sizing: border-box;
+            width: 850px!important;
+            height: 80vh!important;
+            display: flex!important;
+            flex-direction: column!important;
+            gap: 10px!important;
+            padding: 10px!important;
+            box-sizing: border-box!important;
         }
         .gallery img{
-            width: 100%;
-            height: auto;
-            border-radius: 1px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            width: 100%!important;
+            height: auto!important;
+            border-radius: 1px!important;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1)!important;
         }
         .line{
-            margin-top: 1px;
-            width: 100%;
-            height: 3px;
-            background-color: red;
+            margin-top: 1px!important;
+            width: 100%!important;
+            height: 3px!important;
+            background-color: red!important;
         }
     </style>
 </head>
